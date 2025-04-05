fee3134e3fa88864112e5e2c5054826adf6cf016
Tyler7x1 <jpm022504@gmail.com>
2025-04-06 01:42:55 +0530

    Fixed redis implementation

--- redis.js
+++ redis.js
@@ -1,14 +1,31 @@
+import dotenv from "dotenv";
 import Redis from "ioredis";
-const redis = new Redis();
 
-const exTime = 60 * 60 * 6 // 6 hours
+dotenv.config();
+
+const CLOUD_REDIS_URL = process.env.REDIS_URL;
+const exTime = 60 * 60 * 6; // 6 hours
+
+if (!CLOUD_REDIS_URL) {
+  throw new Error("REDIS_URL is not set in environment variables");
+}
+
+const redis = new Redis(CLOUD_REDIS_URL);
+
+redis.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
 
 function setter(key, value) {
-    return redis.set(key, value, 'EX', exTime);
+  return redis.set(key, value, "EX", exTime);
 }
 
 function getter(key) {
-    return redis.get(key);
+  return redis.get(key);
 }
 
 export { setter, getter };

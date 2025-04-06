ce3bcd4b264c55e7d1a1a63787611c53148c125c
Tyler7x1 <jpm022504@gmail.com>
2025-04-06 23:07:22 +0530

    minor fix

--- connect.js
+++ connect.js
@@ -3,13 +3,11 @@ import mongoose from 'mongoose';
 async function connect(url){
     try {
         await mongoose.connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             ssl: true
         });
         console.log('Connected to MongoDB');
     } catch {
-        console.error("Error connecting to MongoDB", err);
+        throw Error;
     }
 }
 

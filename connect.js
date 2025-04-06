7d75e4828c4197cacfc0558418369c6810b35731
Tyler7x1 <jpm022504@gmail.com>
2025-04-06 23:20:50 +0530

    minor fix

--- connect.js
+++ connect.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
 
-async function connect(url){
-    try {
-        await mongoose.connect(url, {
-            ssl: true
-        });
-        console.log('Connected to MongoDB');
-    } catch {
-        throw Error;
-    }
+async function connect(url) {
+  try {
+    await mongoose.connect(url, {
+      ssl: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err.message);
+    throw err;
+  }
 }
 
 export { connect };
